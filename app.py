a2122e7c1dcd75ffd772aab94edbb1095766133d
Elena Battiston <battiston.elena@gmail.com>
2024-03-27 16:49:59 +0000

    final version without map

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import requests
 from datetime import datetime, timedelta
+import pandas as pd
 
 city_couples = {
+    "--": "--",
     "Zurich": "Lugano",
     "Lugano": "Zurich",
     "Innsbruck": "Bolzano",
@@ -10,20 +12,20 @@ city_couples = {
 }
 
 
-st.cache_data
+#st.cache_data
 def get_city_data(end_date):
     start_date = end_date - timedelta(days= 20)
     url = f"https://jawp-7s7ugr6oya-ew.a.run.app/predict"
-    if st.button('air pressure'):
-        params = {'_start' : start_date, '_end': end_date}
-        response = requests.get(url, params=params)
-        if response.status_code == 200:
-            data = response.json()
-            st.write(data)
-            return data
-        else:
-            st.error("Failed to retrieve data for the city :sob: ")
-            return None
+    params = {'_start' : start_date.strftime("%Y-%m-%d"),
+              '_end': end_date.strftime("%Y-%m-%d")}
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        #st.write(data)
+        return data
+    else:
+        st.error("Failed to retrieve data for the city :sob: ")
+        return None
 
 # Function to calculate the difference between two cities' values
 def calculate_difference(value1, value2):
@@ -31,59 +33,57 @@ def calculate_difference(value1, value2):
 
 # Function to determine the risk level based on the difference
 def determine_risk_level(difference):
-    if abs(sum(difference) / 3) > 4:
-        return "Be careful, Foehn risk! :dash:"
-    else:
-        return "It looks safe up there! :+1:"
+    if abs(sum(difference) / 3) < 4:
+        st.success("It looks safe up there! :parachute: ")
+    elif abs(sum(difference) / 3) > 4 and abs(sum(difference) / 3) < 15:
+        st.warning("Be careful, Föhn risk! :tornado: ")
+    elif abs(sum(difference) / 3) > 15:
+        st.error("Don't fly! :skull_and_crossbones: ")
 
 # Streamlit app
 def main():
-    st.title("Foehn predictions for paragliders")
-    #can add a short description
+    st.title("Föhn Warning :parachute: ")
+    st.markdown("""
+    #### Live forecast for paragliding pilots
+
+    This tool provides a real-time forecast for the next 3 hours. All values are expressed in hectopascals (hPa).
+    The threshold for safe flight is set to ±4 hPa, aligning with the criteria for the Föhn phenomenon.
+    """)
 
     selected_city = st.selectbox("Select a city:", list(city_couples.keys()))
+    if selected_city != '--':
+        # Get the couple city
+        couple_city = city_couples[selected_city]
 
-    # Get the couple city
-    couple_city = city_couples[selected_city]
+        st.write(f"You selected **{selected_city}** and its twin :two_men_holding_hands: city **{couple_city}**")
+        selected_date = datetime.today()
 
-    st.write(f"You selected {selected_city} and its twin :two_men_holding_hands: {couple_city}")
+        st.write("Retrieving data...")
 
-
-    selected_date = st.date_input("Select a date for prediction", datetime.today())
-
-    st.write("Retrieving data...")
-
-    # Fetch data for both cities
-    allcity_data = get_city_data(selected_date)
-    #st.write(allcity_data)
-
-
-    city1_data = allcity_data[selected_city]
-    city2_data = allcity_data[couple_city]
+        # Fetch data for both cities
+        allcity_data = get_city_data(selected_date)
+        city1_data = allcity_data[selected_city]
+        city2_data = allcity_data[couple_city]
 
 
-    #st.write(city1_data)
+        if city1_data is not None and city2_data is not None:
+            # Convert each float to string with two decimal places
 
+            city1_data_rounded = [round(num, 2) for num in city1_data]
+            city2_data_rounded = [round(num, 2) for num in city2_data]
 
-    if city1_data is not None and city2_data is not None:
-        # Convert each float to string with two decimal places
-        formatted_data_city1 = [f"{pressure:.2f}" for pressure in city1_data]
-        formatted_string1 = ", ".join(formatted_data_city1)
-        st.write(f"3 hours pressure forecast(hPa) for {selected_city}: {formatted_string1}")
-        #st.write(f"Forecasted pressure(hPa) for {selected_city}: {city1_data}")
-        formatted_data_city2 = [f"{pressure:.2f}" for pressure in city2_data]
-        formatted_string2 = ", ".join(formatted_data_city2)
-        st.write(f"3 hours pressure forecast(hPa) for {couple_city}: {formatted_string2}")
+            difference = calculate_difference(city1_data, city2_data)
 
-        difference = calculate_difference(city1_data, city2_data)
-        formatted_difference = [f"{diff:.2f}" for diff in difference]
-        formatted_difference_string = ", ".join(formatted_difference)
-        st.write(f"Forecasted hPa difference between {selected_city} and {couple_city}: {formatted_difference_string}")
+            difference_rounded = [round(num, 2) for num in difference]
+            # showing the warning first
+            determine_risk_level(difference)
+        else:
+            st.write("Failed to fetch data from the API. Please try again later.")
 
-        risk_level = determine_risk_level(difference)
-        st.write(risk_level)
-    else:
-        st.write("Failed to fetch data from the API. Please try again later.")
+        # Creating dataframe for the table
+        df = pd.DataFrame([city1_data_rounded, city2_data_rounded, difference_rounded], index = [selected_city, couple_city, 'Difference'], columns = ['+1 hour', '+2 hours', '+3 hours']).T
+        st.write('*"Ensuring thorough meteorological checks is essential for achieving safe and joyful landings."* :face_with_monocle: Dr. Zephyr Skywatcher')
+        st.write(df)
 
 
 main()
